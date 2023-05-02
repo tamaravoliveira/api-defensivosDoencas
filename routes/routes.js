816afc169e97b391537696ae8686014cfe8f152f
Tamara Oliveira <tamaravaoliveira@gmail.com>
2023-05-02 13:37:06 -0300

    alterando arquivos

--- routes/routes.js
+++ routes/routes.js
@@ -4,28 +4,7 @@ const mongoose = require('mongoose');
 const Defensivo = require('./models/defensivo');
 const Doenca = require('./models/doenca');
 
-const app = express();
-const port = 3000;
-
-app.use(express.json());
-app.use(cors());
-
-mongoose.connect('mongodb://localhost:27017/agro', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-  console.log('Conectado ao MongoDB!');
-});
-
-const router = express.Router();
-
 //Rotas para defensivos
-
 //Retorna todos os defensivos
 router.get('/defensivos', async (req, res) => {
   const defensivos = await Defensivo.find();
@@ -113,10 +92,4 @@ router.put('/doencas/:id', async (req, res) => {
 router.delete('/doencas/:id', async (req, res) => {
   await Doenca.findByIdAndDelete(req.params.id);
   res.send({ message: 'Doença removida com sucesso' });
-});
-
-app.use('/api', router);
-
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
 });
